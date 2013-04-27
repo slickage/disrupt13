d8e5658bb50cf11c56477c9ac86048203a2cc27d
James Wang <jw@slickage.com>
2013-04-27 13:05:51 -0400

    Initial iOS.

---
+++ Disrupt13/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Disrupt13
+//
+//  Created by James Wang on 4/27/13.
+//  Copyright (c) 2013 TC Disrupt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Disrupt13/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Disrupt13
+//
+//  Created by James Wang on 4/27/13.
+//  Copyright (c) 2013 TC Disrupt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
