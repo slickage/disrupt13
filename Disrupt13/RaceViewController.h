c2fffb3df08314399fe782a9e5e6485a6d3c7682
James Wang <jw@slickage.com>
2013-04-28 05:03:57 -0400

    Race view.

---
+++ Disrupt13/RaceViewController.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+@interface RaceViewController : UIViewController <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ Disrupt13/RaceViewController.m
@@ -0,0 +1,40 @@
+#import "RaceViewController.h"
+
+@interface RaceViewController ()
+
+@end
+
+@implementation RaceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+  //NSString *urlAddress = @"http://www.google.com";
+	NSString *urlAddress = @"http://sandbox.glympse.com/!reusetestuuid";
+	//Create a URL object.
+	NSURL *url = [NSURL URLWithString:urlAddress];
+	
+	//URL Requst Object
+	NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+	
+	//Load the request in the UIWebView.
+	[_webView loadRequest:requestObj];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
