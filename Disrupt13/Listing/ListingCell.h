812bf80bccc1255967c46b215ab57f861b403853
Julie Ann Sakuda <jsakuda@gmail.com>
2013-04-27 20:37:17 -1000

    Custom listing cell.

---
+++ Disrupt13/Listing/ListingCell.h
@@ -0,0 +1,13 @@
+//
+//  ListingCell.h
+//  Disrupt13
+//
+//  Created by Julie Ann Sakuda on 4/27/13.
+//  Copyright (c) 2013 TC Disrupt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListingCell : UITableViewCell
+
+@end
